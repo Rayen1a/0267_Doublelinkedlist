2558748d66db6abfc1672a280a43179282015645
Rayen1a <raihan01426@gmail.com>
2025-05-16 08:36:38 +0700

    Memasukkan class DoubleLinkList public & private

--- file1.cpp
+++ file1.cpp
@@ -9,3 +9,15 @@ int noMhs;
 Node *next;
 Node *prev;
 };
+
+class DoubleLinkedList
+{
+    private:
+    Node *START;
+
+    public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+};    
