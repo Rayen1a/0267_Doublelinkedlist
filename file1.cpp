ea6275f491db7a334b62bc3c14a6a56a3dc369d9
Rayen1a <raihan01426@gmail.com>
2025-05-16 09:52:14 +0700

    Memasukkan step2

--- file1.cpp
+++ file1.cpp
@@ -154,4 +154,22 @@ void revtraverse()
         cout << "\nList is empty" << endl;
         return;
     }
+    // Step 1: Plove to last node
+    Node *currentNode = START;
+    int i = 0;
+    while (currentNode->next != NULL)
+    {
+        currentNode = currentNode->next;
+        i++;
+    }
+    
+    // Step 2: Traverse backward
+    cout << "\nRecords in descending order of roll number are:\n"; 
+    while (currentNode != NULL)
+    {
+        cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+        // Step 3: Move to previous node
+        currentNode currentNode->prev;
+        1--;
+    }
 }
