e9d2b2510859ff29cadc6df8bf2e30852846e836
Rayen1a <raihan01426@gmail.com>
2025-05-16 09:44:38 +0700

    Memasukkan step2

--- file1.cpp
+++ file1.cpp
@@ -132,4 +132,17 @@ void traverse()
     cout << "\nList is empty" << endl;
     return;
     }
+    // Step 1: Mark first node as currentNode
+    Node *currentNode = START;
+    
+    // Step 2: Repeat until currentNode == NULL
+    cout << "\nRecords in ascending order of roll number are:\n";
+    int i=0;
+    while (currentNode != NULL)
+    {
+        cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+        // Step 3: Move to next nod
+        currentNode currentNode->next;
+        i++;
+    }
 }
